ad5113767314ba60c697c9e2cdbec014efaef723
Cottonwood <blog_geon@naver.com>
2022-03-24 17:01:16 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
 function App() {
-  return <></>;
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {() => (
+            <ul>
+              <Draggable draggableId="first" index={0}>
+                {() => <li>Hello</li>}
+              </Draggable>
+
+              <Draggable draggableId="second" index={1}>
+                {() => <li>Hello</li>}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
 }
 
 export default App;
